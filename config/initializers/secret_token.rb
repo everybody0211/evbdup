7489c2b254d560af0cd4ba179ffc637ab1f46ab5
evbd <evbd@dhuo-VirtualBox.(none)>
2014-11-28 12:16:18 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Evbdup::Application.config.secret_key_base = '113942b9ff5668aa7fcf9520fa1c8aa67cde09084ed149b9104e5654479c3970bebe7b9ebdae0d0d7ac035eded79b8e923325ad05d809636ee4a8b61e440028a'
